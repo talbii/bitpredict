364bcbb477fbaecf885ef5d5152dc00e227f568d
talbii <ido@tal.bi>
2022-01-31 20:33:21 +0200

    Added currency option for requesting coin prices

--- data/base.py
+++ data/base.py
@@ -5,22 +5,23 @@ class IAPIGetter(ABC):
     """
     Interface for retrieving coin prices from APIs.
     """
-    def __init__(self, url: str):
+    def __init__(self, url: str, default_currency: str = None):
         self.api_url = url
+        self.default_currency = default_currency
 
     @abstractmethod
-    def get_latest_price(self, coin: str) -> float:
+    def get_latest_price(self, coin: str, currency: str = None) -> float:
         pass
 
     @abstractmethod
-    def get_interval_price(self, coin: str) -> List[float]:
+    def get_interval_price(self, coin: str, currency: str = None) -> List[float]:
         pass
 
     @abstractmethod
-    def get_latest_prices(self, coin: List[str]) -> Dict[str, float]:
+    def get_latest_prices(self, coin: List[str], currency: str = None) -> Dict[str, float]:
         pass
 
     @abstractmethod
-    def get_interval_prices(self, coin: List[str]) -> Dict[str, List[float]]:
+    def get_interval_prices(self, coin: List[str], currency: str = None) -> Dict[str, List[float]]:
         pass
 
