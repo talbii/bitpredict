a31730e50faff50700e0180d5c7809dbb971044c
talbii <ido@tal.bi>
2022-02-04 12:42:18 +0200

    Added APIGetterExceptions class

--- data/base.py
+++ data/base.py
@@ -1,11 +1,15 @@
 from abc import ABC, abstractmethod
 from typing import List, Dict
 
+class BitPredictException(Exception):
+    pass
+
 class IAPIGetter(ABC):
     """
     Interface for retrieving coin prices from APIs.
     """
-    def __init__(self, url: str, default_currency: str = None):
+    def __init__(self, api_key: str, url: str, default_currency: str = None):
+        self.api_key = api_key
         self.api_url = url
         self.default_currency = default_currency
 
@@ -25,3 +29,14 @@ class IAPIGetter(ABC):
     def get_interval_prices(self, coin: List[str], currency: str = None) -> Dict[str, List[float]]:
         pass
 
+class TimeInterval:
+    def __init__(self):
+        raise NotImplemented()
+
+class APIGetterExceptions:
+    class NoCurrency(BitPredictBaseException):
+        pass
+
+    class InvalidRequest(BitPredictBaseException):
+        def __init__(self, coin: str, currency: str):
+            super(coin, currency)
