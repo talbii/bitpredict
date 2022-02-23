d1d8268b6d591c8baea3c3779280627d7261f97f
talbii <ido@tal.bi>
2022-02-23 22:21:06 +0200

    Tidied up update_coins

--- functions/index.js
+++ functions/index.js
@@ -5,13 +5,6 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 const { symbol } = require("./symbol.js");
 var yf = require('yahoo-finance');
 
@@ -23,21 +16,25 @@ async function get_latest_quote(s) {
 
     return data.price.regularMarketPrice;
 }
+
 exports.update_coins = functions.pubsub.schedule('every 5 minutes').onRun((context) => {
     const fs = admin.firestore();
     let batch = fs.batch();
     return (async () => {
+        // get all documents and read them
         await fs.collection("data").listDocuments().then(docs => {
         return fs.getAll(...docs);
     }).then(async snapshots => {
-        for(let ds of snapshots) {
-            if(ds.exists) {
+        for(let ds of snapshots) { 
+            // for each document snapshot
+            if(ds.exists) { // safe check; this should never enter
+                // get the data from the snapshot and process it
                 let data = ds.data();
                 functions.logger.info(`Got data of ${ds.id}: ${JSON.stringify(data)}`);
                 data.historical.push(data.latest);
                 functions.logger.info(`Symbol: ${symbol.get(ds.id)}`);
                 let quote = await get_latest_quote(symbol.get(ds.id)).then(result => {
-                    functions.logger.info(`finished resolve of yf: ${result}`)
+                    functions.logger.info(`Finished resolve of yf for ${ds.id}: ${result}`)
                     data.latest = result;
                     functions.logger.info(`Setting data of ${ds.id}: ${JSON.stringify(data)}`);
                     batch.update(ds.ref, data);
