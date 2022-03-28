0d454deb7adc1e18d811987b2efe75c36468eca7
talbii <ido@tal.bi>
2022-03-28 08:56:16 +0300

    Implemented Database class

---
+++ bitpredict/app/src/main/java/com/talbii/bitpredict/Database.java
@@ -0,0 +1,133 @@
+package com.talbii.bitpredict;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+
+public class Database {
+    public final String TAG = "Database";
+    private static final long MEGABYTE = 1024 * 1024;
+    private final FirebaseFirestore db;
+    private final StorageReference icons;
+    public final static Set<String> availableCoins;
+    static {
+        GetCoinsRunnable g = new GetCoinsRunnable();
+        Thread t = new Thread(g);
+        t.start();
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException("(Database) Error in getting list of coins (static block)");
+        }
+
+        availableCoins = Collections.unmodifiableSet(new HashSet<>(g.getAvailableCoins()));
+        Log.d("Database/static_block", "Got list of available coins: " + availableCoins);
+    }
+
+    public Database () {
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build();
+        db = FirebaseFirestore.getInstance();
+        db.setFirestoreSettings(settings);
+        icons = FirebaseStorage.getInstance().getReference().child("ic");
+    }
+
+    /*Gets the historical and latest prices of coin s, and places them in c.
+    *  Assumes s is in availableCoins.*/
+    private CoinStruct getQuote(@NonNull String s) {
+
+        GetCoinRunnable g = new GetCoinRunnable(db, s);
+        Thread t = new Thread(g);
+        t.start();
+        try {
+            t.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException("(Database) Error in getting list of coins (static block)");
+        }
+
+        return g.getCoinStruct();
+    }
+
+    public CoinStruct getCoin(String s) {
+        if(!availableCoins.contains(s)) return null;
+
+        CoinStruct c = getQuote(s);
+        c.iconref = icons.child(s + ".png");
+        //c.icon = getIcon(s, c.iconref);
+        return c;
+    }
+
+    private static class GetCoinsRunnable implements Runnable {
+        private ArrayList<String> a;
+
+        public GetCoinsRunnable() {
+            a = new ArrayList<>();
+        }
+
+        @Override
+        public void run() {
+            try {
+                this.a = (ArrayList<String>) Tasks.await(FirebaseFirestore.getInstance().collection("info").document("coins").get()).get("all");
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+                throw new RuntimeException("(Database) Error in getting list of coins (static block)");
+            }
+        }
+
+        public ArrayList<String> getAvailableCoins() {
+            return a;
+        }
+    }
+
+    private static class GetCoinRunnable implements Runnable {
+        private final CoinStruct c;
+        private final FirebaseFirestore fs;
+        private final String s;
+
+        public GetCoinRunnable(FirebaseFirestore f, String symbol) {
+            c = new CoinStruct();
+            fs = f;
+            s = symbol;
+        }
+
+        @Override
+        public void run() {
+            try {
+                var dref = fs.collection("data").document(s);
+                var ds = Tasks.await(dref.get());
+                if(ds.exists()) {
+                    c.name = (String) ds.get("name");
+                    c.latest = (double) ds.get("latest");
+                    c.historical = (DocumentReference) ds.get("historical");
+                }
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public CoinStruct getCoinStruct() {
+            return c;
+        }
+    }
+}
